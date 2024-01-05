57f533b9541f27b81b39f6f27e6a64d51e1b5fc7
Cláudio Santana <claudio_bs@icloud.com>
2024-01-05 17:10:53 -0300

    feat: entrega exercicios EBAC módulo 22

--- testing-cy-bdd/cypress/integration/Login/login.steps..js
+++ testing-cy-bdd/cypress/integration/Login/login.steps..js
@@ -1,14 +1,15 @@
 /// <reference types="cypress"/>
 
-import { Given, When, Then } from 'cypress-cucumber-preprocessor'
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 const { dashboardPage } = require('../../support/pages')
+const { login } = require('../../support/commands')
 
 Given('I visit EBAC Store', () => {
     cy.visit('/minha-conta')
 })
 
 When("I log in with user {string} and pass {string}", (user, pass) => {
-    cy.login(data.username, data.password)
+    cy.login(user, pass)
 })
 
 Then('the user dashboard page should be visible', () => {
