7c6e7a3d612ec364845d54aefe8e8433650959b0
Cláudio Santana <claudio_bs@icloud.com>
2024-01-27 16:17:23 -0300

    feat: initial commit

--- testing-cy-bdd/cypress/support/pages/dashboard.page.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="cypress"/>
-
-export const dashboardPage = {
-    get siteTitle() { return cy.get('.page-title')},
-    get checkName() { return cy.get('.woocommerce-MyAccount-content > :nth-child(2)')},
-    get mensage() { return cy.get('.woocommerce-MyAccount-content > :nth-child(3)')}
-}
-
--- testing-cy-bdd/cypress/support/pages/index.js
+++
@@ -1,4 +0,0 @@
-import { dashboardPage } from './dashboard.page'
-import loginPage from './login.page';
-
-export { dashboardPage, loginPage}
